bf474133be6ab008dfd142778de4f021ed2705ac
Ni <j.ni@student.tue.nl>
2018-03-07 12:57:05 +0100

    test

--- src/SimpleEstimator.cpp
+++ src/SimpleEstimator.cpp
@@ -143,10 +143,7 @@ void SimpleEstimator::calculate(uint32_t label, bool inverse) {
                 }
             }
         }
-
-        // assume uniform distributed.
-        divider = divider / 4;
-
+        
         //Ts = current cardStat1.noPaths
         // apply the formula: new noPhts = Tr * Ts / Max(V(R,Y), V(S,Y))
         cardStat1.noPaths = cardStat1.noPaths * edges.size() /  divider;
