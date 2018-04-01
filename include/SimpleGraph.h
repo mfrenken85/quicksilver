5b7ffa7acda0dd93d531baf1f4a5c49e57726a47
Mark Frenken <mfrenken85@gmail.com>
2018-04-01 12:21:16 +0200

    Revert "Merge pull request #1 from mfrenken85/datastruct_test"
    This reverts commit 9d81fed6b9174744198ad3a86dbfc6ce64450284, reversing
    changes made to 041aa662ac923cb87bf93065ccaef9bf912744c9.

--- include/SimpleGraph.h
+++ include/SimpleGraph.h
@@ -8,7 +8,6 @@
 #include <unordered_map>
 #include <unordered_set>
 #include <vector>
-#include <map>
 #include <iostream>
 #include <regex>
 #include <fstream>
@@ -16,11 +15,8 @@
 
 class SimpleGraph : public Graph {
 public:
-
     std::vector<std::vector<std::pair<uint32_t,uint32_t>>> adj;
     std::vector<std::vector<std::pair<uint32_t,uint32_t>>> reverse_adj; // vertex adjacency list
-
-
 protected:
     uint32_t V;
     uint32_t L;
@@ -41,49 +37,6 @@ public:
 
     void setNoVertices(uint32_t n);
     void setNoLabels(uint32_t noLabels);
-
-    // linked list
-
-
-    struct AdjListNode
-    {
-        uint32_t to;
-        struct AdjListNode *next; // pointer
-    };
-
-// A structure to represent an adjacency list
-    struct AdjList
-    {
-        uint32_t from;
-        struct AdjListNode *head;  // pointer to head node of list
-        struct AdjList *next;
-    };
-
-
-    struct AdjTable
-    {
-        uint32_t label; // table name
-        uint32_t V; // no vertices
-        uint32_t E; // no edges
-        struct AdjList *head;
-        struct AdjTable *next;
-    };
-
-
-    void addEdgeToLinkedList(uint32_t from, uint32_t to, uint32_t edgeLabel, AdjTable *table, bool reverse);
-    AdjTable* createTableHead();
-    AdjTable* getTable(uint32_t label, bool reverse); // get from tablePointers
-    void setTable(uint32_t label, AdjTable* table, bool reverse); // set pointers to tablePointers
-
-
-    // linked list
-    AdjTable *tableHead;
-    AdjTable *reverse_tableHead;
-
-    // tablePointers[label, std::make_pair[adj, rev_adj]]
-    std::map<uint32_t,AdjTable*> tablePointers;
-    std::map<uint32_t,AdjTable*> reverse_tablePointers;
-
 
 };
 
